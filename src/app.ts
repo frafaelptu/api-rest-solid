0c7b8d4aa104f0f612fcd2749ae6a7d85ede61d9
francisco.gomes <francisco.gomes@grupocasasbahia.com.br>
2024-06-16 05:46:49 -0300

    features: Refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify'
+import { fastifyCookie } from '@fastify/cookie'
 import { usersRoutes } from './http/controllers/users/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
@@ -9,8 +10,16 @@ import { checkInsRoutes } from './http/controllers/check-ins/routes'
 export const app = fastify()
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
 
+app.register(fastifyCookie)
 app.register(usersRoutes)
 app.register(gymsRoutes)
 app.register(checkInsRoutes)
